c782e1dc939423767f04186f66eb8a4b911dad53
MahmoudElshourbagy1 <mahmoudmera555@gmail.com>
2026-02-08 23:03:04 +0200

    Add user delete and password change features to API
    Expanded AppUserController with delete and password change endpoints.
    Added command handlers, models, and validation for these actions.
    Updated resource files, routes, and keys for improved error messaging and localization.

--- SchoolProject.Core/Features/AppUser/Commands/Handllers/UserCommandHandler.cs
+++ SchoolProject.Core/Features/AppUser/Commands/Handllers/UserCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 using SchoolProject.Core.Bases;
 using SchoolProject.Core.Features.AppUser.Commands.Models;
@@ -11,26 +12,29 @@ namespace SchoolProject.Core.Features.AppUser.Commands.Handllers
 {
     public class UserCommandHandler : ResponseHandler,
         IRequestHandler<AddUserCommand, Response<string>>,
-        IRequestHandler<EditUserCommand, Response<string>>
+        IRequestHandler<EditUserCommand, Response<string>>,
+        IRequestHandler<DeleteUserCommand, Response<string>>,
+        IRequestHandler<ChangeUserPasswordCommand, Response<string>>
+
 
     {
         private readonly IMapper _mapper;
-        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
+        private readonly IStringLocalizer<SharedResources> _sharedResources;
         private readonly UserManager<User> _userManager;
         public UserCommandHandler(IStringLocalizer<SharedResources> stringLocalizer, IMapper mapper, UserManager<User> userManager) : base(stringLocalizer)
         {
             _mapper = mapper;
-            _stringLocalizer = stringLocalizer;
+            _sharedResources = stringLocalizer;
             _userManager = userManager;
         }
         public async Task<Response<string>> Handle(AddUserCommand request, CancellationToken cancellationToken)
         {
             //if Email or UserName exist
             var Email = await _userManager.FindByEmailAsync(request.Email);
-            if (Email != null) return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.EmailIsExist]);
+            if (Email != null) return BadRequest<string>(_sharedResources[SharedResourcesKeys.EmailIsExist]);
 
             var user = await _userManager.FindByNameAsync(request.UserName);
-            if (user != null) return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.UserIsExist]);
+            if (user != null) return BadRequest<string>(_sharedResources[SharedResourcesKeys.UserIsExist]);
             //mapping
             var identityUser = _mapper.Map<User>(request);
             //Create Email
@@ -47,18 +51,49 @@ namespace SchoolProject.Core.Features.AppUser.Commands.Handllers
             //Check if user is exist
             var olduser = await _userManager.FindByIdAsync(request.Id.ToString());
             //Not found
-            if (olduser == null)
-            {
-                return NotFound<string>();
-            }
+            if (olduser == null) return NotFound<string>();
+
             //Mapping
             var newuser = _mapper.Map(request, olduser);
+            //if user is Exist
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == newuser.UserName && x.Id != newuser.Id);
+            if (user != null) return BadRequest<string>(_sharedResources[SharedResourcesKeys.UserIsExist]);
             //Update
             var result = await _userManager.UpdateAsync(newuser);
             //result is not success
-            if (!result.Succeeded) return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.UpdateFailed]);
+            if (!result.Succeeded) return BadRequest<string>(_sharedResources[SharedResourcesKeys.UpdateFailed]);
             //massage
-            return Success((string)_stringLocalizer[SharedResourcesKeys.Updated]);
+            return Success((string)_sharedResources[SharedResourcesKeys.Updated]);
+        }
+
+        public async Task<Response<string>> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            //Check if user is exist
+            var user = await _userManager.FindByIdAsync(request.Id.ToString());
+            //Not found
+            if (user == null)
+            {
+                return NotFound<string>();
+            }
+            //Delete
+            var result = await _userManager.DeleteAsync(user);
+            if (!result.Succeeded) return BadRequest<string>(_sharedResources[SharedResourcesKeys.DeletedFailed]);
+            return Success((string)_sharedResources[SharedResourcesKeys.Deleted]);
+
+
+        }
+
+        public async Task<Response<string>> Handle(ChangeUserPasswordCommand request, CancellationToken cancellationToken)
+        {
+            //get user
+            var user = await _userManager.FindByIdAsync(request.Id.ToString());
+            //if not exist return not found
+            if (user == null) return NotFound<string>();
+            //Change User Password
+            var result = await _userManager.ChangePasswordAsync(user, request.CurrentPassword, request.NewPassword);
+            //result
+            if (!result.Succeeded) return BadRequest<string>(result.Errors.FirstOrDefault().Description);
+            return Success((string)_sharedResources[SharedResourcesKeys.Success]);
         }
     }
 }
---
+++ SchoolProject.Core/Features/AppUser/Commands/Validatiors/ChangeUserPasswordValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using SchoolProject.Core.Features.AppUser.Commands.Models;
+using SchoolProject.Core.Resources;
+
+namespace SchoolProject.Core.Features.AppUser.Commands.Validatiors
+{
+    public class ChangeUserPasswordValidator : AbstractValidator<ChangeUserPasswordCommand>
+    {
+        private readonly IStringLocalizer<SharedResources> _localizer;
+        public ChangeUserPasswordValidator(IStringLocalizer<SharedResources> localizer)
+        {
+            _localizer = localizer;
+            ApplyValidationRules();
+            ApplyCustomValidationRules();
+
+        }
+        public void ApplyValidationRules()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage(_localizer[SharedResourcesKeys.NotEmpty])
+                 .NotNull().WithMessage(_localizer[SharedResourcesKeys.RequiredField]);
+
+            RuleFor(x => x.CurrentPassword)
+                .NotEmpty().WithMessage(_localizer[SharedResourcesKeys.NotEmpty])
+                .NotNull().WithMessage(_localizer[SharedResourcesKeys.RequiredField]);
+
+            RuleFor(x => x.NewPassword)
+                .NotEmpty().WithMessage(_localizer[SharedResourcesKeys.NotEmpty])
+                 .NotNull().WithMessage(_localizer[SharedResourcesKeys.RequiredField]);
+
+            RuleFor(x => x.ConfirmPassword)
+           .Equal(x => x.NewPassword).WithMessage(_localizer[SharedResourcesKeys.PassNotEqualConfrimPass]);
+
+        }
+        public void ApplyCustomValidationRules()
+        {
+
+
+        }
+
+    }
+}
